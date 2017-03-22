9c32e64262642d964cb264b790a17ef02c2f9ca6
antosha <nishtak97@mail.ru>
2017-03-22 17:44:34 +0300

    kdtree

--- k-d_tree/main.cpp
+++ k-d_tree/main.cpp
@@ -10,16 +10,16 @@ int SHIFT = 0;
 template <typename T>
 class Node{
 private:
-    T elem;
-    std::vector<Node*> children;
+    T elem;// тип данных в вершине
+    std::vector<Node*> children; //в векторе чилдрен храним указатели на детей
 public:
     Node(T);
     ~Node();
     void insert(T);
-    void remove(T);
-    Node<T>* find(T);
-    void dump();
-    T get_elem();
+    void remove(T);//удаляет всех детей и вершину
+    Node<T>* find(T); // находит элемент по значению и возвращает указатель на элемент
+    void dump();// печать
+    T get_elem();// возвращает значение поля элемента
 };
 
 template <typename T>
@@ -29,27 +29,27 @@ Node<T>::Node(T _elem) {
 
 template <typename T>
 Node<T>::~Node() {
-    while(children.size() != 0){
-        delete children.back();
-        children.pop_back();
+    while(children.size() != 0){// пока есть дети удаляем детей
+        delete children.back();// дает значение последнего элемента в векторе
+        children.pop_back(); // удаляет последний элемент вектора
     }
 }
 
 
 template <typename T>
 void Node<T>::insert(T _elem) {
-    children.push_back(new Node<T>(_elem));
+    children.push_back(new Node<T>(_elem));//добавить объект класса ноуд
 }
 
 template <typename T>
 Node<T>* Node<T>::find(T _elem) {
     Node<T>* res = NULL;
     if (elem == _elem) {
-        return this;
+        return this; // вовзращаем указательна объект
     } else {
-        for(int i = 0; i < children.size(); i++){
+        for(int i = 0; i < children.size(); i++){ // функция вектора, которая возвращает размер вектора
             if (res == NULL) {
-                res = children[i]->find(_elem);
+                res = children[i]->find(_elem); //
             } else {
                 return res;
             }
@@ -59,18 +59,18 @@ Node<T>* Node<T>::find(T _elem) {
 }
 
 template <typename T>
-void Node<T>::dump() {
+void Node<T>::dump() { // печать
     for(int i = 0; i < SHIFT; i++){
         std::cout<< '\t';
     }
-    std::cout<<elem;
+    std::cout<<elem;// вывели на экран элемент
     if (children.size() > 0) {
-        int temp = SHIFT;
+        int temp = SHIFT; // временная переменная temp
         SHIFT = 1;
-        children[0]->dump();
+        children[0]->dump();// вызвали печать от 0 ребенка
         if(children.size() > 1)
-            SHIFT = temp + 1;
-            for (int i = 1; i < children.size(); i++) {
+            SHIFT = temp + 1;// увеличиваем табьуляцию если есть дети
+            for (int i = 1; i < children.size(); i++) {// печатаем остальных детей
                 children[i]->dump();
             }
             SHIFT--;
@@ -81,9 +81,9 @@ void Node<T>::dump() {
 template <typename T>
 void Node<T>::remove(T _elem) {
     for(int i = 0; i < children.size(); i++){
-        if(children[i]->get_elem() == _elem){
-            delete children[i];
-            children.erase(children.begin() + i);
+        if(children[i]->get_elem() == _elem){ //если значение ребенка совпадает со значением, которое надо удалить, то удаляем его
+            delete children[i]; // вызвали деструктор, удалили детей ребенка
+            children.erase(children.begin() + i);//удаляется элемент и схлопывается массив
             return;
         }
     }
@@ -94,7 +94,7 @@ T Node<T>::get_elem() {
 }
 
 int main(){
-    Node<int>* tree = new Node<int>(10);
+    Node<int>* tree = new Node<int>(10);//создали дерево,объявляем тип шаблона, выделили память под деревом
     tree->insert(9);
     tree->insert(8);
     tree->insert(7);
@@ -112,6 +112,7 @@ int main(){
     tree->remove(8);
 
     tree->dump();
+
 
     delete tree;
     return 0;
