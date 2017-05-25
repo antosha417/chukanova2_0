2d8c93c113bd13da96fc3d88d386283bf334a9b8
antosha <nistak97@mail.ru>
2017-05-25 14:58:47 +0300

    mayan_numbers

---
+++ dichotomy/main.cpp
@@ -0,0 +1,241 @@
+//
+// Created by antosha on 25.05.17.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::ostream;
+using std::vector;
+
+
+class mayan_number{
+public:
+    mayan_number(){};
+    mayan_number(const char*);
+    mayan_number(int);
+
+    mayan_number operator=(const mayan_number& num);
+    mayan_number operator+(const mayan_number& num);
+    mayan_number operator-(const mayan_number& num);
+    mayan_number operator*(const mayan_number& num);
+    mayan_number operator*(int num);
+    mayan_number operator/(const mayan_number& num);
+    bool operator>(const mayan_number& num);
+    bool operator<(const mayan_number& num);
+    bool operator==(const mayan_number& num);
+
+    friend ostream& operator<<(ostream& os, const mayan_number& num);
+private:
+    vector<int> number;
+};
+
+ostream& operator<<(ostream& os, const mayan_number& num){
+    for (int i = (int)num.number.size() - 1; i > -1; --i) {
+        os<<'('<<num.number[i]<<')';
+    }
+    return os;
+}
+
+mayan_number::mayan_number(const char * str) {
+    while(*str != '\0'){
+        if(*str != '(') {
+            cout<<"Init error";
+            number.clear();
+            return;
+        }
+        str++;
+        int res = 0;
+        int count = 0;
+        while(*str == '.'){
+            res++;
+            str++;
+            count++;
+        }
+        if(count > 4) {
+            cout<<"Init error";
+            number.clear();
+            return;
+        }
+        count = 0;
+        while(*str == '|'){
+            res+=5;
+            str++;
+            count++;
+        }
+        if(count > 4) {
+            cout<<"Init error";
+            number.clear();
+            return;
+        }
+        if(*str != ')') {
+            cout<<"Init error";
+            number.clear();
+            return;
+        }
+        str++;
+        if(res > 19) {
+            cout<<"Init error";
+            number.clear();
+            return;
+        }
+        number.push_back(res);
+    }
+    std::reverse(number.begin(), number.end());
+}
+
+mayan_number mayan_number::operator=(const mayan_number &num) {
+    this->number=num.number;
+    return *this;
+}
+
+mayan_number mayan_number::operator+(const mayan_number &num) {
+    mayan_number res;
+    int i = 0;
+    int mem = 0;
+    while (true) {
+        int sum = mem;
+        if (i < num.number.size())
+            sum += num.number[i];
+        if (i < number.size())
+            sum += number[i];
+        mem = sum/20;
+        res.number.push_back(sum % 20);
+        if (i >= num.number.size() && i >= number.size()) {
+            res.number.push_back(mem);
+            while(res.number.back() == 0)
+                res.number.pop_back();
+            break;
+        }
+        i++;
+    }
+    return res;
+}
+
+mayan_number mayan_number::operator-(const mayan_number &num) {
+    mayan_number res;
+    int i = 0;
+    int mem = 0;
+    while (true) {
+        int sum = mem;
+        if (i < num.number.size())
+            sum -= num.number[i];
+        if (i < number.size())
+            sum += number[i];
+        if (sum < 0) {
+            mem = -1;
+            sum = 20 + sum;
+        } else {
+            mem = 0;
+        }
+        res.number.push_back(sum);
+        if (i >= num.number.size() && i >= number.size()) {
+            if (mem == -1){
+                res.number.clear();
+                //cout<<"Mayans didn't know negative numbers"<<endl;
+                return res;
+            }
+            while(res.number.back() == 0 && res.number.size() != 1)
+                res.number.pop_back();
+            break;
+        }
+        i++;
+    }
+    return res;
+}
+
+mayan_number mayan_number::operator*(const mayan_number &num) {
+    mayan_number res("()");
+    for (int i = 0; i < number.size(); ++i) {
+        mayan_number temp;
+        for (int j = 0; j < i; ++j) {
+            temp.number.push_back(0);
+        }
+        int mem = 0;
+        for (int k = 0; k < num.number.size(); ++k) {
+            int mul = number[i]*num.number[k] + mem;
+            mem = mul/20;
+            temp.number.push_back(mul%20);
+        }
+
+        while (mem != 0) {
+            temp.number.push_back(mem%20);
+            mem/=20;
+        }
+        res = res + temp;
+    }
+    return res;
+}
+
+mayan_number mayan_number::operator/(const mayan_number &num) {
+    mayan_number num_copy = num;
+    if(*this == num) {
+        return mayan_number(1);
+    }
+    if(*this < num) {
+        return mayan_number(0);
+    }
+    mayan_number res;
+    mayan_number part;
+    int i = 0;
+    while (i < number.size()) {
+        part.number.clear();
+        part.number.push_back(number[i++]);
+        while(part < num) {
+            part.number.push_back(number[i++]);
+            res.number.push_back(0);
+        }
+        int part_res = 1;
+        while( num_copy * part_res < part)
+            part_res++;
+        res.number.push_back(--part_res);
+    }
+    std::reverse(res.number.begin(), res.number.end());
+    while(res.number.back() == 0 && res.number.size() != 1)
+        res.number.pop_back();
+    return res;
+}
+
+bool mayan_number::operator>(const mayan_number &num) {
+    mayan_number temp = *this - num;
+    return temp.number.size() != 0 && temp.number[0] != 0;
+}
+
+mayan_number::mayan_number(int num) {
+    while (num != 0) {
+        number.push_back(num%20);
+        num /= 20;
+    }
+}
+
+mayan_number mayan_number::operator*(int num) {
+    return mayan_number(num)*(*this);
+}
+
+bool mayan_number::operator==(const mayan_number &num) {
+    mayan_number temp = *this - num;
+    return temp.number.size() == 1 && temp.number[0] == 0;
+}
+
+bool mayan_number::operator<(const mayan_number &num) {
+    mayan_number temp = *this - num;
+    return temp.number.size() == 0;
+}
+
+int main() {
+    mayan_number a("(.)(..)");
+
+
+    mayan_number b("(..)(.)");
+
+    mayan_number c (210);
+    mayan_number d (210);
+    mayan_number q = a;
+    cout<<c/d<<endl;
+
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,129 +1,25 @@
 #include <iostream>
-#include <string>
-#include <stdlib.h>
-#include <set>
-#include <cstring>
-
 using namespace std;
 
-class Word : public string {
-
-    unsigned long nbk;
-
-    unsigned long count();
-
-    void sort();
-
+class Date
+{
+    int mo, da, yr;
 public:
-    Word();
-
-    Word(string &);
-
-    Word(char *);
-
-    Word(char);
-
-    Word *operator=(string);
-
-    Word *operator=(const char *);
-
-    Word *operator=(char);
-
-    Word &operator*(int);
-
-    Word &operator!();
-
-    Word &operator/(int);
-
-    Word &operator-(Word &);
-
-    void instead(Word &, Word &);
-
-    unsigned long getLet();
-};
-
-Word::Word() {
-    nbk = 0;
-};
-
-Word::Word(string &a) : string(a) {
-    nbk = count();
-};
-
-Word::Word(char *str) : string(str) {
-    nbk = count();
-};
-
-Word::Word(char str) {
-    *(std::string *) this = str;
-    nbk = count();
-};
-
-unsigned long Word::getLet() {
-    return nbk;
-};
-
-unsigned long Word::count() {
-    return ((std::string *) this)->length();
-};
-
-Word *Word::operator=(string a) {
-    *(std::string *) this = string::operator=(a);
-    nbk = count();
-    return this;
-};
-
-Word *Word::operator=(const char *a) {
-    *(std::string *) this = string::operator=(a);
-    nbk = count();
-    return this;
-};
-
-Word *Word::operator=(char a) {
-    *(std::string *) this = string::operator=(a);
-    nbk = 1;
-    return this;
-};
-
-int cmp(const void *a, const void *b) {
-    return *(char *) a - *(char *) b;
-};
-
-// пример сортировки
-void Word::sort() {
-    qsort((void *) (*(std::string *) this).data(), (*(std::string *) this).length(), sizeof(char), cmp);
-};
-
-Word &Word::operator!() {
-    cout<<(((std::string *) this)->size());
-    /*for (int i = 0; i < ((std::string *) this)->length() / 2 - 1; ++i) {
-        char temp = (*this)[i];
-        (*this)[i] = (*this)[((std::string *) this)->length() - i];
-        (*this)[((std::string *) this)->length() - i - 1] = temp;
-    }*/
-};
-
-Word &Word::operator*(int times) {
-    const char *data = c_str();
-    char *new_data = new char(times * ((std::string *) this)->length());
-    for (int i = 0; i < times * ((std::string *) this)->length(); ++i) {
-        new_data[i] = data[i%((std::string *) this)->length()];
+    Date(int m, int d, int y)
+    {
+        mo = m; da = d; yr = y;
     }
-    clear();
-    Word *res = new Word();
-    *res = new_data;
-    return *res;
+    friend ostream& operator<<(ostream& os, const Date& dt);
 };
 
-//Word &operator/(int) {
-//};
+ostream& operator<<(ostream& os, const Date& dt)
+{
+    os << dt.mo << '/' << dt.da << '/' << dt.yr;
+    return os;
+}
 
-int main() {
-    std::string a = "12345";
-    a = 'a';
-    Word b = a;
-    cout << !b << endl;
-    cout << b*3 << endl;
-
-    return 0;
+int main()
+{
+    Date dt(5, 6, 92);
+    cout << dt;
 }
